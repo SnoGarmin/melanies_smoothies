35b2506733c2b28dee489e19df4cfa97ae6ca3dc
SnoGarmin <garmin@global.co.za>
2026-01-28 08:26:48 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,8 +1,8 @@
 # Import python packages
 import streamlit as st
-#from snowflake.snowpark.context import get_active_session                       # Remove this line for Streamlit Not in Snowflake
+# from snowflake.snowpark.context import get_active_session                       # Remove this line for Streamlit Not in Snowflake
 
-#New section to display smoothiefroot nutrition information   API Requests
+# New section to display smoothiefroot nutrition information   API Requests
 import requests
 
 
@@ -28,11 +28,7 @@ my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT
 
 ingredients_list = st.multiselect('Choose up to 5 ingredients:', my_dataframe, max_selections=5)
 
-# API Requests
-#==============
-smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")
-#st.text(smoothiefroot_response.json())
-sf_df = st.dataframe(data=smoothiefroot_response.json(),use_container_width=True)
+
 
 #st.write(ingredients_list)
 #st.text(ingredients_list)
@@ -49,6 +45,8 @@ if ingredients_list:
 
  for fruit_chosen in ingredients_list:
     ingredients_string += fruit_chosen + ' '
+    smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")
+    sf_df = st.dataframe(data=smoothiefroot_response.json(),use_container_width=True)
 
  #st.write(ingredients_string)
 
